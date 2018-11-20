f4847833be9268088365da7147955ced1b61d004
digifort <aseem.kher@gmail.com>
2018-11-21 02:38:54 +0530

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var moment = require("moment");
+var unique = require("uniq");
 var myDate = new Date();
-
-console.log(myDate);
+var myCoolDate = moment(myDate).format('LL');
+var mylist = [1,2,1,1,1,3,3,4,5,6,4,4,7];
+var myUniqueList = unique(mylist);
+console.log(myCoolDate);
+console.log(myUniqueList);
